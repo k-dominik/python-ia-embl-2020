66af309e01f0ac8d691891eaec32928b5d7bfa1e
k-dominik <k-dominik@users.noreply.github.com>
2020-01-11 23:24:40 +0100

    small fixes to make tests run

--- exercises/03-RootSlice.py
+++ exercises/03-RootSlice.py
@@ -2,15 +2,11 @@ import skimage.io
 import skimage.viewer
 
 # load and display original image
-image = skimage.io.imread(fname="../data/roots.jpg")
+image = skimage.io.imread(fname="../data/roots.tif")
 viewer = skimage.viewer.ImageViewer(image)
 viewer.show()
 
 # extract, display, and save sub-image
 # WRITE YOUR CODE TO SELECT THE SUBIMAGE NAME clip HERE:
-
-viewer = skimage.viewer.ImageViewer(clip)
-viewer.show()
-
 
 # WRITE YOUR CODE TO SAVE clip HERE
